957460586513b64fc53310c188992bda2489af2d
odm275 <pomejia@gmail.com>
2020-08-29 22:34:08 -0500

    correct tag button

--- src/components/tags/TechTag.js
+++ src/components/tags/TechTag.js
@@ -10,8 +10,8 @@ const TechTag = (props) => {
         <div className="d-inline-block p-1">
             <Link to={`/tags/${tag}/`}>
                 <button
-                    className="tech-tag text-white">
-                    <p className="d-inline">{tech} </p>
+                    className="tech-tag text-white d-flex align-items-center">
+                    <div className="d-inline" style={{marginRight: '4px'}}>{tech} </div>
                     <div className="d-inline" style={{ fontSize: size, color: color }}><svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" width={size} height={size} style={{fill: `${color}`}}>
                             <title>{tech}</title>
                             <path
