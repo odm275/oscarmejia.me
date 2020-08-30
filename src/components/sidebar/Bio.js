cd4d7838ea9dc386ab9a110b5995fc46cc09598d
odm275 <pomejia@gmail.com>
2020-08-29 19:26:31 -0500

    added my pictures and tweaked a few parts

--- src/components/sidebar/Bio.js
+++ src/components/sidebar/Bio.js
@@ -1,13 +1,28 @@
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
 import "./sidebar.css"
 
-import willjw3 from "../../images/willjw3.jpg"
+import Img from "gatsby-image"
 
 const Bio = ({ author, tagline }) => {
-
+    const data = useStaticQuery(graphql`
+    query {
+      file(relativePath: { eq: "2020vision.jpg" }) {
+        childImageSharp {
+          # Specify a fixed image and fragment.
+          # The default width is 400 pixels
+          fixed(width: 150, height: 150) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
+    }
+  `);
+  console.log('data:', data)
     return (
         <div className="bio-main w-75">
-            <img src={willjw3} style={{ maxWidth: `100px` }} className="profile-img" alt="" />
+            <Img fixed={data.file.childImageSharp.fixed} className="profile-img" alt="profile image"/> 
+            {/* <img src={myPicture} style={{ maxWidth: `100px` }} className="profile-img" alt="" /> */}
             <h3 className="mt-2 author-bio">{author}</h3>
             <small className="text-muted">{tagline}</small>
         </div>
--- src/pages/about.js
+++ src/pages/about.js
@@ -2,7 +2,7 @@ import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import { FaCheckCircle } from "react-icons/fa"
+import { FaRocket, FaCheckCircle } from "react-icons/fa"
 import "./index.css"
 
 import Sidebar from "../components/sidebar/Sidebar"
@@ -32,44 +32,38 @@ const AboutPage = (props) => {
                     <SEO title="About" />
                     <div className="mt-3">
                         <h2 className="heading">About</h2>
-                        <p><i>Developer Diary is a Gatsby Starter blog template created with web developers in mind, but really, anyone can use it. It's totally usable right out of the box, but minimalist enough to be easily modifiable to suit your needs.</i></p>
+                        <p><i>Welcome to my Blog!! My posts are about topics I'm intersted it in or that I think will help otehrs!</i></p>
                         <br />
-                        <h4>Features</h4>
+                        <h4>Topics I'll write about:</h4>
+                        <div>
+                            <span className="text-success d-inline-block" title="prism">
+                                <FaCheckCircle size={26} style={{ color: "success" }} />
+                            </span>
+                            <p className="d-inline-block ml-3 w-75 align-top">Building Apps/Websites and Features.</p>
+                        </div>
                         <div>
                             <span className="text-success d-inline-block" title="blazing">
                                 <FaCheckCircle size={26} style={{ color: "success" }} />
                             </span>
-                            <p className="d-inline-block ml-3 w-75 align-top">Blazing fast, as you'd expect from a Gatsby site</p>
+                            <p className="d-inline-block ml-3 w-75 align-top">Solutions to annoying bugs I have come across.</p>
+                        </div>
+                        <div>
+                            <span className="text-success d-inline-block" title="icons">
+                                <FaCheckCircle size={26} style={{ color: "success" }} />
+                            </span>
+                            <p className="d-inline-block ml-3 w-75 align-top">Personal Opinions on Software Development as a career based on my experience in the industry.</p>
                         </div>
                         <div>
                             <span className="text-success d-inline-block" title="tags">
-                                <FaCheckCircle size={26} style={{ color: "success" }} />
+                                <FaRocket size={26} style={{ color: "gray" }} />
                             </span>
-                            <p className="d-inline-block ml-3 w-75 align-top">Tech tags designed for web developers</p>
+                            <p className="d-inline-block ml-3 w-75 align-top">Find Categories you are intersted in:</p>
                             <div className="ml-5">
                                 <TechTag tag="react" tech="React" name={tags["react"]} size={20} color="deepskyblue" />
                                 <TechTag tag="nodejs" tech="Node.js" name={tags["nodejs"]} size={20} color="lightgreen" />
                                 <TechTag tag="html" tech="HTML" name={tags["html"]} size={20} color="darkorange" />
                                 <TechTag tag="css" tech="CSS" name={tags["css"]} size={20} color="teal" />
                             </div>  
-                        </div>
-                        <div className="mt-4">
-                            <span className="text-success d-inline-block" title="prism">
-                                <FaCheckCircle size={26} style={{ color: "success" }} />
-                            </span>
-                            <p className="d-inline-block ml-3 w-75 align-top">Includes Prism for code block styling in markdown files</p>
-                        </div>
-                        <div>
-                            <span className="text-success d-inline-block" title="icons">
-                                <FaCheckCircle size={26} style={{ color: "success" }} />
-                            </span>
-                            <p className="d-inline-block ml-3 w-75 align-top">Developer-relevant social-media icon links including GitHub, Stack Overflow and freeCodeCamp</p>
-                        </div>
-                        <div>
-                            <span className="text-success d-inline-block" title="mobile">
-                                <FaCheckCircle size={26} style={{ color: "success" }} />
-                            </span>
-                            <p className="d-inline-block ml-3 w-75 align-top">Mobile responsive, of course</p>
                         </div>
                     </div>
                 </div>
